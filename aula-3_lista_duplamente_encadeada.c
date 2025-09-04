346c2d6dcbde974f47a74396be7311ead120119e
BADAYA1 <102997039+BADAYA1@users.noreply.github.com>
2025-09-04 01:37:46 +0000

    aula 3 acabando?

--- aula-3_lista_duplamente_encadeada.c
+++ aula-3_lista_duplamente_encadeada.c
@@ -10,7 +10,9 @@ typedef struct Celula{
 typedef struct{
     int qtde;
     Celula *primeiro;
+    Celula *ultimo;
 }LDDE;
+
 
 void inserir(LDDE *lista, int dado){
     Celula *nova = malloc(sizeof(Celula));
@@ -30,32 +32,57 @@ void inserir(LDDE *lista, int dado){
     if(atual == NULL && anterior == NULL){  //lista vazia
         lista->primeiro = nova;
     }
-    lista->qtde++;
+    else if(atual == NULL && anterior != NULL){ //lista cheia
+        anterior->proximo = nova;
+        nova->anterior = anterior;
+    }
+    else if (atual != NULL && anterior != NULL){//inserir no meio
+        anterior->proximo = nova;
+        nova->proximo = atual;
+        nova->anterior = anterior;
+        atual->anterior = nova;
+    }
+    
+    Celula *ultimo = nova->valor;
 
+    lista->qtde++;
 }
 
 void imprimir(LDDE *lista){
     printf("[ ");
     Celula *atual = lista->primeiro;
-    while(atual != NULL){
+    int i = 0;
+    while(atual != NULL && i < lista->qtde){
         printf("%d ", atual->valor);
+        atual = atual->proximo;
+        i++;
     }
+    printf("]\n");
+}
 
-
-    printf("]");
-    
+void imprimirInverso(LDDE *lista){
+    printf("[ ");
+    Celula *atual = lista->ultimo;
+    int i = 0;
+    while(atual != NULL && i < lista->qtde){
+        printf("%d ", atual->valor);
+        atual = atual->anterior;
+        i++;
+    }
+    printf("]\n");
 }
 
 int main(){
     LDDE *lista = malloc(sizeof(LDDE));
     lista->qtde = 0;
     lista->primeiro = NULL;
-    printf("ÇLista duplamente encadeada");
+    printf("Lista duplamente encadeada\n");
     inserir(lista, 1);
     inserir(lista, 2);
     inserir(lista, 3);
     inserir(lista, 5);
     inserir(lista, 7);
     imprimir(lista);
+    imprimirInverso(lista);
     return 0;
 }

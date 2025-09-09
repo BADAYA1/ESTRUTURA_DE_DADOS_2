660e57548062c549906e1a75d1646d1ac65b586d
BADAYA1 <102997039+BADAYA1@users.noreply.github.com>
2025-09-09 21:45:41 +0000

    aula 3 acabado B)

--- aula-3_lista_duplamente_encadeada.c
+++ aula-3_lista_duplamente_encadeada.c
@@ -7,16 +7,29 @@ typedef struct Celula{
     struct Celula *anterior;
 }Celula;
 
-typedef struct{
+typedef struct LDDE{
     int qtde;
     Celula *primeiro;
-    Celula *ultimo;
 }LDDE;
 
+LDDE *inicializa_lista(){
+	LDDE *lista = malloc(sizeof(LDDE));
+	lista->primeiro = NULL;
+	lista->qtde = 0;
+	return lista;
+}
 
-void inserir(LDDE *lista, int dado){
+Celula *inicializa_celula(int valor){
+	Celula *celula = malloc(sizeof(Celula));
+	celula->anterior = NULL;
+	celula->proximo = NULL;
+	celula->valor = valor;
+	return celula;
+}
+
+void inserir(LDDE *lista, int valor){
     Celula *nova = malloc(sizeof(Celula));
-    nova->valor = dado;
+    nova->valor = valor;
     nova->proximo = NULL;
     nova->anterior = NULL;
 
@@ -24,7 +37,14 @@ void inserir(LDDE *lista, int dado){
     Celula *atual = lista->primeiro;
     Celula *anterior = NULL;
 
-    while(atual != NULL && atual->valor <= dado){
+    /*if(atual != NULL && anterior == NULL && atual->valor > valor){
+        atual->anterior = atual;
+        nova->anterior = nova;
+
+        
+    }*/
+
+    while(atual != NULL && atual->valor <= valor){
         anterior = atual;
         atual = atual->proximo;
     }
@@ -42,48 +62,90 @@ void inserir(LDDE *lista, int dado){
         nova->anterior = anterior;
         atual->anterior = nova;
     }
+    else if(atual != NULL && anterior == NULL){ /*INICIO DA LISTA*/
+        lista->primeiro = nova;
+        nova->proximo = atual;
+        atual->anterior = nova;
+    }
     
-    Celula *ultimo = atual;
+    //Celula *ultimo = atual;
 
     lista->qtde++;
 }
 
-void imprimir(LDDE *lista){
-    printf("[ ");
+void imprimir_crescente(LDDE *lista){
     Celula *atual = lista->primeiro;
-    int i = 0;
-    while(atual != NULL && i < lista->qtde){
+    while(atual != NULL){
         printf("%d ", atual->valor);
         atual = atual->proximo;
-        i++;
     }
-    printf("]\n");
+    printf("\n");
 }
 
-void imprimirInverso(LDDE *lista){
-    printf("[ ");
-    Celula *atual = lista->ultimo;
-    int i = 0;
-    while(atual != NULL && i < lista->qtde){
-        printf("%d ", atual->valor);
-        atual = atual->anterior;
-        i++;
-    }
-    printf("]\n");
+void imprimir_decrescente(LDDE *lista){
+    Celula *atual = lista->primeiro;
+Celula *anterior = NULL;
+while(atual != NULL){
+anterior = atual; //anterior avança 1
+atual = atual->proximo;
+}
+while(anterior != NULL){
+printf("%d ", anterior->valor);
+anterior = anterior->anterior;
+}
+printf("\n");
 }
 
-int main(){
-    LDDE *lista = malloc(sizeof(LDDE));
-    lista->qtde = 0;
-    lista->primeiro = NULL;
-    lista->ultimo = NULL;
-    printf("Lista duplamente encadeada\n");
-    inserir(lista, 1);
-    inserir(lista, 2);
-    inserir(lista, 3);
-    inserir(lista, 5);
-    inserir(lista, 7);
-    //imprimir(lista);
-    imprimirInverso(lista);
-    return 0;
+Celula *buscar(LDDE *lista, int valor){
+Celula *atual = lista->primeiro;
+while (atual != NULL && atual->valor != valor){ //fica no while até o atual->valor == valor e retorna o atual(valor a ser buscado)
+atual = atual->proximo;
+}
+return atual;
+}
+
+void remover(LDDE *lista, int valor){
+Celula *atual = buscar(lista, valor); //como eu tenho o atual sendo a celula com o valor que quero remover posso fazer a logica de troca de ponteiros
+if(atual == NULL) { /*LISTA VAZIA*/
+return;
+}
+else if(atual->proximo == NULL){ /*FIM DA LISTA*/
+if(atual->anterior != NULL){ //verifica se nao tem só essa celula na lista
+atual->anterior->proximo = NULL;
+}else{
+lista->primeiro = NULL;
+
+}
+}
+else if(atual->proximo != NULL && atual->anterior != NULL){ /*MEIO DA LISTA*/
+atual->anterior->proximo = atual->proximo; //coloca o ponteiro da celula anterior proximo no atual proximo
+atual->proximo->anterior = atual->anterior;
+}
+else if(atual != NULL && atual->anterior == NULL){ /*INICIO DA LISTA*/
+lista->primeiro = atual->proximo;
+if(atual->proximo != NULL){
+atual->proximo->anterior = NULL;
+}
+}
+free(atual);
+lista->qtde--;
+}
+
+int main(void) {
+  LDDE *lista = inicializa_lista();
+	int in[] = {2, 3, 9, 6, 7, 4, 1, 8, 0, 5};
+	int out[] = {0, 9, 1, 8, 5, 5, 6, 2, 3, 4, 7};
+	int len_in = sizeof(in) / sizeof(int);
+	int len_out = sizeof(out) / sizeof(int);
+	for(int i = 0; i < len_in; i ++){
+		inserir(lista, in[i]);
+		imprimir_crescente(lista);
+		imprimir_decrescente(lista);
+	}
+	for(int j = 0; j < len_out; j++){
+	    remover(lista, out[j]);
+		imprimir_crescente(lista);
+		imprimir_decrescente(lista);
+	}
+  return 0;
 }
